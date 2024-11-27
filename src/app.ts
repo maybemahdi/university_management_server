3f6bfba33bd280984121be2add80281b72cba3ae
Mahdi Hasan Mahi <mh7266391@gmail.com>
2024-11-27 13:43:07 +0600

    initiated

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+const app: Application = express();
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+// application routes
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Car Store is Running");
+});
+
+export default app;
